a8be5cb6a0bae3712937ac4c807ceb523a846fa5
Muhammad Taha <127325759+MuhammadTaha01@users.noreply.github.com>
2023-03-12 16:54:53 +0500

    Add files via upload

---
+++ 5.23.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout<<"Enter number of weekdays: ";
+    cin>>n;
+
+    switch(n)
+    {
+      case 1:
+          cout<<"Friday";
+          break;
+      case 2:
+           cout<<"Saturday";
+           break;
+      case 3:
+           cout<<"Sunday";
+           break;
+      case 4:
+           cout<<"Monday";
+           break;
+      case 5:
+           cout<<"Tuesday";
+           break;
+      case 6:
+           cout<<"Wednesday";
+           break;
+
+      default:
+        cout<<"Invalid number";
+
+    return 0;
+
+    }
+}
